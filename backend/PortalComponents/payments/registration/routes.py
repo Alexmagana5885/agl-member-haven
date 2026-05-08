4e409369bac43f08add044c0fe390b8496055289
Alexmagana5885 <maganaalex634@gmail.com>
2026-05-09 01:51:08 +0300

    commit

--- backend/PortalComponents/payments/registration/routes.py
+++ backend/PortalComponents/payments/registration/routes.py
@@ -27,10 +27,11 @@ MPESA_CALLBACK_URL = os.environ.get(
     "REGISTRATION_CALLBACK_URL",
     "https://member.log.agl.or.ke/members/forms/Payment/callback.php"
 )
-MPESA_ENVIRONMENT = os.environ.get("ENVIRONMENT", "sandbox")
+MPESA_ENVIRONMENT = os.environ.get("MPESA_ENVIRONMENT", "sandbox")
 
 MPESA_PARTY_B = os.environ.get("MPESA_PARTY_B", "8209382")
-MPESA_ACCOUNT_REFERENCE = os.environ.get("MPESA_ACCOUNT_REFERENCE", "6175135")
+# MPESA_ACCOUNT_REFERENCE = os.environ.get("MPESA_ACCOUNT_REFERENCE_REGISTRATION", "6175135")
+MPESA_ACCOUNT_REFERENCE = os.environ["MPESA_ACCOUNT_REFERENCE_REGISTRATION"]
 
 # STK URLs
 if MPESA_ENVIRONMENT == "production":
