25195e6e859494600836a0344c7717853adcb1c4
Alexmagana5885 <maganaalex634@gmail.com>
2026-05-15 22:56:02 +0300

    commit

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,8 +1,9 @@
-# passenger_wsgi.py
-
-import sys
+import imp
 import os
+import sys
+
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-from app import app as application
+wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
+application = wsgi.application
